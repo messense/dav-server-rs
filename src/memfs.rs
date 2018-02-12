746599ee10e46f64814192ba78a3a1e0f00b8e0c
Miquel van Smoorenburg <mike@langeraar.net>
2018-02-12 21:15:26 +0100

    initial memfs code

---
+++ src/memfs.rs
@@ -0,0 +1,430 @@
+//
+//  Simple in-memory filesystem.
+//
+use std;
+use std::io::{self,Read,Write,Seek,SeekFrom};
+use std::io::Result as IoResult;
+use std::time::SystemTime;
+use std::io::{Error,ErrorKind};
+use std::sync::{Arc,Mutex};
+
+use super::webpath::WebPath;
+use super::fs::*;
+
+use tree;
+
+type Tree = tree::Tree<Vec<u8>, MemFsNode>;
+
+#[derive(Debug)]
+pub struct MemFs {
+    tree:   Arc<Mutex<Tree>>,
+}
+
+#[derive(Debug,Clone)]
+enum MemFsNode {
+    Dir(MemFsDirNode),
+    File(MemFsFileNode),
+}
+
+#[derive(Debug,Clone)]
+struct MemFsDirNode {
+    mtime:      SystemTime,
+}
+
+#[derive(Debug,Clone)]
+struct MemFsFileNode {
+    mtime:      SystemTime,
+    data:       Vec<u8>,
+}
+
+#[derive(Debug)]
+struct MemFsReadDir {
+    iterator:   std::vec::IntoIter<MemFsDirEntry>
+}
+
+#[derive(Debug,Clone)]
+struct MemFsDirEntry {
+    mtime:      SystemTime,
+    is_dir:     bool,
+    name:       Vec<u8>,
+    size:       u64,
+}
+
+#[derive(Debug)]
+struct MemFsFile {
+    tree:       Arc<Mutex<Tree>>,
+    node_id:    u64,
+    pos:        usize,
+    append:     bool,
+}
+
+impl MemFs {
+    pub fn new() -> Box<MemFs> {
+        let root = MemFsNode::new_dir();
+        Box::new(MemFs{tree: Arc::new(Mutex::new(Tree::new(root)))})
+    }
+
+    fn do_open(&self, tree: &mut Tree, path: &[u8], options: OpenOptions) -> FsResult<Box<DavFile>> {
+        let node_id = match tree.lookup(path) {
+            Ok(n) => {
+                if options.create_new {
+                    return Err(FsError::Exists);
+                }
+                n
+            },
+            Err(FsError::NotFound) => {
+                if !options.create {
+                    return Err(FsError::NotFound);
+                }
+                let parent_id = tree.lookup_parent(path)?;
+                tree.add_child(parent_id, file_name(path), MemFsNode::new_file(), true)?
+            },
+            Err(e) => return Err(e),
+        };
+        let node = tree.get_node_mut(node_id).unwrap();
+        if node.is_dir() {
+            return Err(FsError::Forbidden);
+        }
+        if options.truncate {
+            node.as_file_mut()?.data.truncate(0);
+            node.update_mtime(SystemTime::now());
+        }
+        Ok(Box::new(MemFsFile{
+            tree:       self.tree.clone(),
+            node_id:    node_id,
+            pos:        0,
+            append:     options.append,
+        }))
+    }
+}
+
+impl Clone for MemFs {
+    fn clone(&self) -> Self {
+        MemFs{
+            tree: Arc::clone(&self.tree),
+        }
+    }
+}
+
+impl DavFileSystem for MemFs {
+
+    // boilerplate helper so that clone() works.
+    fn box_clone(&self) -> Box<DavFileSystem> {
+        Box::new((*self).clone())
+    }
+
+    fn metadata(&self, path: &WebPath) -> FsResult<Box<DavMetaData>> {
+        let tree = &*self.tree.lock().unwrap();
+        let node_id = tree.lookup(path.as_bytes())?;
+        Ok(Box::new(tree.get_node(node_id)?.as_dirent(path.as_bytes())))
+    }
+
+    fn read_dir(&self, path: &WebPath) -> FsResult<Box<DavReadDir<Item=Box<DavDirEntry>>>> {
+        let tree = &*self.tree.lock().unwrap();
+        let node_id = tree.lookup(path.as_bytes())?;
+        if !tree.get_node(node_id)?.is_dir() {
+            return Err(FsError::Forbidden);
+        }
+        let mut v = Vec::new();
+        for (name, dnode_id) in tree.get_children(node_id)? {
+            if let Ok(node) = tree.get_node(dnode_id) {
+                v.push(node.as_dirent(&name));
+            }
+        }
+        Ok(Box::new(MemFsReadDir{
+            iterator: v.into_iter(),
+        }))
+    }
+
+    fn open(&self, path: &WebPath, options: OpenOptions) -> FsResult<Box<DavFile>> {
+        let tree = &mut *self.tree.lock().unwrap();
+        self.do_open(tree, path.as_bytes(), options)
+    }
+
+    fn create_dir(&self, path: &WebPath) -> FsResult<()> {
+        debug!("FS: create_dir {:?}", path);
+        let tree = &mut *self.tree.lock().unwrap();
+        let path = path.as_bytes();
+        let parent_id = tree.lookup_parent(path)?;
+        tree.add_child(parent_id, file_name(path), MemFsNode::new_dir(), false)?;
+        tree.get_node_mut(parent_id)?.update_mtime(SystemTime::now());
+        Ok(())
+    }
+
+    fn remove_file(&self, path: &WebPath) -> FsResult<()> {
+        let tree = &mut *self.tree.lock().unwrap();
+        let parent_id = tree.lookup_parent(path.as_bytes())?;
+        let node_id = tree.lookup(path.as_bytes())?;
+        tree.delete_node(node_id)?;
+        tree.get_node_mut(parent_id)?.update_mtime(SystemTime::now());
+        Ok(())
+    }
+
+    fn remove_dir(&self, path: &WebPath) -> FsResult<()> {
+        let tree = &mut *self.tree.lock().unwrap();
+        let parent_id = tree.lookup_parent(path.as_bytes())?;
+        let node_id = tree.lookup(path.as_bytes())?;
+        tree.delete_node(node_id)?;
+        tree.get_node_mut(parent_id)?.update_mtime(SystemTime::now());
+        Ok(())
+    }
+
+    fn rename(&self, from: &WebPath, to: &WebPath) -> FsResult<()> {
+        let tree = &mut *self.tree.lock().unwrap();
+        let node_id = tree.lookup(from.as_bytes())?;
+        let parent_id = tree.lookup_parent(from.as_bytes())?;
+        let dst_id = tree.lookup_parent(to.as_bytes())?;
+        tree.move_node(node_id, dst_id, file_name(to.as_bytes()), true)?;
+        tree.get_node_mut(parent_id)?.update_mtime(SystemTime::now());
+        tree.get_node_mut(dst_id)?.update_mtime(SystemTime::now());
+        Ok(())
+    }
+
+    fn copy(&self, from: &WebPath, to: &WebPath) -> FsResult<()> {
+        let tree = &mut *self.tree.lock().unwrap();
+
+        // source must exist.
+        let snode_id = tree.lookup(from.as_bytes())?;
+
+        // make sure destination exists, create if needed.
+        {
+            let mut oo = OpenOptions::write();
+            oo.create = true;
+            self.do_open(tree, to.as_bytes(), oo)?;
+        }
+        let dnode_id = tree.lookup(to.as_bytes())?;
+
+        // copy.
+        let data = (*tree.get_node_mut(snode_id)?).clone();
+        *tree.get_node_mut(dnode_id)? = data;
+
+        Ok(())
+    }
+}
+
+impl DavReadDir for MemFsReadDir {}
+
+impl Iterator for MemFsReadDir {
+    type Item = Box<DavDirEntry>;
+
+    fn next(&mut self) -> Option<Box<DavDirEntry>> {
+        match self.iterator.next() {
+            Some(entry) => Some(Box::new(entry)),
+            None => None,
+        }
+    }
+}
+
+impl DavDirEntry for MemFsDirEntry {
+
+    fn metadata(&self) -> FsResult<Box<DavMetaData>> {
+        Ok(Box::new((*self).clone()))
+    }
+
+    fn name(&self) -> Vec<u8> {
+        self.name.clone()
+    }
+
+    //fn is_dir(&self) -> FsResult<bool> { Ok(self.is_dir()) }
+}
+
+impl DavMetaData for MemFsDirEntry {
+    fn len(&self) -> u64 {
+        self.size
+    }
+
+    fn modified(&self) -> FsResult<SystemTime> {
+        Ok(self.mtime)
+    }
+
+    fn is_dir(&self) -> bool {
+        self.is_dir
+    }
+}
+
+impl DavFile for MemFsFile {
+    fn metadata(&self) -> Box<DavMetaData> {
+        let tree = &*self.tree.lock().unwrap();
+        // XXX FIXME unwrap ... need to return FsResult<> here
+        let node = tree.get_node(self.node_id).unwrap();
+        Box::new(node.as_dirent(b""))
+    }
+}
+
+impl Read for MemFsFile {
+    fn read(&mut self, buf: &mut [u8]) -> IoResult<usize> {
+        let tree = &*self.tree.lock().unwrap();
+        let node = tree.get_node(self.node_id).map_err(fserror_to_ioerror)?;
+        let file = node.as_file().map_err(fserror_to_ioerror)?;
+        let curlen = file.data.len();
+        let mut start = self.pos;
+        let mut end = self.pos + buf.len();
+        if start > curlen { start = curlen }
+        if end > curlen { end = curlen }
+        let cnt = end - start;
+        buf[..cnt].copy_from_slice(&file.data[start..end]);
+        Ok(cnt)
+    }
+}
+
+impl Write for MemFsFile {
+    fn write(&mut self, buf: &[u8]) -> IoResult<usize> {
+        let tree = &mut *self.tree.lock().unwrap();
+        let node = tree.get_node_mut(self.node_id).map_err(fserror_to_ioerror)?;
+        let file = node.as_file_mut().map_err(fserror_to_ioerror)?;
+        let start = if self.append { file.data.len() } else { self.pos };
+        let end = start + buf.len();
+        if end > file.data.len() {
+            file.data.resize(end, 0);
+        }
+        file.data[start..end].copy_from_slice(buf);
+        Ok(end - start)
+    }
+
+    fn flush(&mut self) -> IoResult<()> {
+        Ok(())
+    }
+}
+
+impl Seek for MemFsFile {
+    fn seek(&mut self, pos: SeekFrom) -> IoResult<u64> {
+        let (start, offset) : (u64, i64) = match pos {
+            SeekFrom::Start(npos) => { self.pos = npos as usize; return Ok(npos) },
+            SeekFrom::Current(npos) => (self.pos as u64, npos),
+            SeekFrom::End(npos) => {
+                let tree = &*self.tree.lock().unwrap();
+                let node = tree.get_node(self.node_id).map_err(fserror_to_ioerror)?;
+                let curlen = node.as_file().map_err(fserror_to_ioerror)?.data.len() as u64;
+                (curlen, npos)
+            },
+        };
+        if offset < 0 {
+            if -offset as u64 > start {
+                return Err(Error::new(ErrorKind::InvalidInput, "invalid seek"));
+            }
+            self.pos = (start - (-offset as u64)) as usize;
+        } else {
+            self.pos = (start + offset as u64) as usize;
+        }
+        Ok((self.pos as u64))
+    }
+}
+
+impl MemFsNode {
+    fn new_dir() -> MemFsNode {
+        MemFsNode::Dir(MemFsDirNode{
+            mtime: SystemTime::now(),
+        })
+    }
+
+    fn new_file() -> MemFsNode {
+        MemFsNode::File(MemFsFileNode{
+            mtime: SystemTime::now(),
+            data: Vec::new(),
+        })
+    }
+
+    // helper to create MemFsDirEntry from a node.
+    fn as_dirent(&self, name: &[u8]) -> MemFsDirEntry {
+        let (is_dir, size, mtime) = match self {
+            &MemFsNode::File(ref file) => (false, file.data.len() as u64, file.mtime),
+            &MemFsNode::Dir(ref dir) => (true, 0, dir.mtime),
+        };
+        MemFsDirEntry {
+            name: name.to_vec(),
+            mtime: mtime,
+            is_dir: is_dir,
+            size: size as u64,
+        }
+    }
+
+    fn update_mtime(&mut self, tm: std::time::SystemTime) {
+        match self {
+            &mut MemFsNode::Dir(ref mut d) => d.mtime = tm,
+            &mut MemFsNode::File(ref mut f) => f.mtime = tm,
+        }
+    }
+
+    fn is_dir(&self) -> bool {
+        match self {
+            &MemFsNode::Dir(_) => true,
+            &MemFsNode::File(_) => false,
+        }
+    }
+
+    fn as_file(&self) -> FsResult<&MemFsFileNode> {
+        match self {
+            &MemFsNode::File(ref n) => Ok(n),
+            _ => Err(FsError::Forbidden),
+        }
+    }
+
+    fn as_file_mut(&mut self) -> FsResult<&mut MemFsFileNode> {
+        match self {
+            &mut MemFsNode::File(ref mut n) => Ok(n),
+            _ => Err(FsError::Forbidden),
+        }
+    }
+}
+
+trait TreeExt {
+    fn lookup_segs(&self, segs: Vec<&[u8]>) -> FsResult<u64>;
+    fn lookup(&self, path: &[u8]) -> FsResult<u64>;
+    fn lookup_parent(&self, path: &[u8]) -> FsResult<u64>;
+}
+
+impl TreeExt for Tree {
+    fn lookup_segs(&self, segs: Vec<&[u8]>) -> FsResult<u64> {
+        let mut node_id = tree::ROOT_ID;
+        let mut is_dir = true;
+        for seg in segs.iter() {
+            if !is_dir {
+                return Err(FsError::Forbidden);
+            }
+            if self.get_node(node_id)?.is_dir() {
+                node_id = self.get_child(node_id, seg.to_vec())?;
+            } else {
+                is_dir = false;
+            }
+        }
+        Ok(node_id)
+    }
+
+    fn lookup(&self, path: &[u8]) -> FsResult<u64> {
+        self.lookup_segs(path.split(|&c| c == b'/').filter(|s| s.len() > 0).collect())
+    }
+
+    // pop the last segment off the path, do a lookup, then
+    // check if the result is a directory.
+    fn lookup_parent(&self, path: &[u8]) -> FsResult<u64> {
+        let mut segs : Vec<&[u8]> = path.split(|&c| c == b'/').filter(|s| s.len() > 0).collect();
+        segs.pop();
+        let node_id = self.lookup_segs(segs)?;
+        if !self.get_node(node_id)?.is_dir() {
+            return Err(FsError::Forbidden);
+        }
+        Ok(node_id)
+    }
+}
+
+// helper
+fn file_name(path: &[u8]) -> Vec<u8> {
+    path.split(|&c| c == b'/').filter(|s| s.len() > 0).last().unwrap_or(b"").to_vec()
+}
+
+// error translation
+fn fserror_to_ioerror(e: FsError) -> io::Error {
+    match e {
+        FsError::NotImplemented => io::Error::new(io::ErrorKind::Other, "NotImplemented"),
+        FsError::GeneralFailure => io::Error::new(io::ErrorKind::Other, "GeneralFailure"),
+        FsError::Exists => io::Error::new(io::ErrorKind::AlreadyExists, "Exists"),
+        FsError::NotFound => io::Error::new(io::ErrorKind::NotFound, "Notfound"),
+        FsError::Forbidden => io::Error::new(io::ErrorKind::PermissionDenied, "Forbidden"),
+        FsError::InsufficientStorage => io::Error::new(io::ErrorKind::Other, "InsufficientStorage"),
+        FsError::LoopDetected => io::Error::new(io::ErrorKind::Other, "LoopDetected"),
+        FsError::PathTooLong => io::Error::new(io::ErrorKind::Other, "PathTooLong"),
+        FsError::TooLarge => io::Error::new(io::ErrorKind::Other, "TooLarge"),
+        FsError::IsRemote => io::Error::new(io::ErrorKind::Other, "IsRemote"),
+    }
+}
+
---
+++ src/tree.rs
@@ -0,0 +1,188 @@
+
+use std;
+use std::collections::HashMap;
+use std::hash::Hash;
+use std::fmt::Debug;
+
+use FsError;
+use FsResult;
+
+#[derive(Debug)]
+/// A tree contains a bunch of nodes.
+pub struct Tree<K: Eq + Hash, D> {
+    nodes:      HashMap<u64, Node<K, D>>,
+    node_id:    u64,
+}
+
+/// id of the root node of the tree.
+pub const ROOT_ID : u64 = 1;
+
+#[derive(Debug)]
+/// Node itself. "data" contains user-modifiable data.
+pub struct Node<K: Eq + Hash, D> {
+    pub data:   D,
+    id:         u64,
+    parent_id:  u64,
+    children:   HashMap<K, u64>
+}
+
+#[derive(Debug)]
+// Iterator over the children of a node.
+pub struct Children<K>(std::vec::IntoIter<(K, u64)>);
+
+impl<K: Eq + Hash + Debug + Clone, D: Debug> Tree<K, D> {
+
+    /// Get new tree and initialize the root with 'data'.
+    pub fn new(data: D) -> Tree<K, D> {
+        let mut t = Tree{
+            nodes:      HashMap::new(),
+            node_id:    ROOT_ID,
+        };
+        t.new_node(99999999, data);
+        t
+    }
+
+    fn new_node(&mut self, parent: u64, data: D) -> u64 {
+        let id = self.node_id;
+        self.node_id += 1;
+        let node = Node{
+            id:         id,
+            parent_id:  parent,
+            data:       data,
+            children:   HashMap::new(),
+        };
+        self.nodes.insert(id, node);
+        id
+    }
+
+    /// add a child node to an existing node.
+    pub fn add_child(&mut self, parent: u64, key: K, data: D, overwrite: bool) -> FsResult<u64> {
+        {
+            let pnode = self.nodes.get(&parent).ok_or(FsError::NotFound)?;
+            if !overwrite && pnode.children.contains_key(&key) {
+                return Err(FsError::Exists);
+            }
+        }
+        let id = self.new_node(parent, data);
+        let pnode = self.nodes.get_mut(&parent).unwrap();
+
+        pnode.children.insert(key, id);
+        Ok(id)
+    }
+
+    /*
+     * unused ...
+    pub fn remove_child(&mut self, parent: u64, key: K) -> FsResult<()> {
+        let id = {
+            let pnode = self.nodes.get(&parent).ok_or(FsError::NotFound)?;
+            let id = *pnode.children.get(&key).ok_or(FsError::NotFound)?;
+            let node = self.nodes.get(&id).unwrap();
+            if node.children.len() > 0 {
+                return Err(FsError::Forbidden);
+            }
+            id
+        };
+        {
+            let pnode = self.nodes.get_mut(&parent).unwrap();
+            pnode.children.remove(&key);
+        }
+        self.nodes.remove(&id);
+        Ok(())
+    }*/
+
+    /// Get a child node by key K.
+    pub fn get_child(&self, parent: u64, key: K) -> FsResult<u64> {
+        let pnode = self.nodes.get(&parent).ok_or(FsError::NotFound)?;
+        let id = pnode.children.get(&key).ok_or(FsError::NotFound)?;
+        Ok(*id)
+    }
+
+    /// Get all children of this node. Returns an iterator over <K, D>.
+    pub fn get_children(&self, parent: u64) -> FsResult<Children<K>> {
+        let pnode = self.nodes.get(&parent).ok_or(FsError::NotFound)?;
+        let mut v = Vec::new();
+        for (k, i) in &pnode.children {
+            v.push(((*k).clone(), *i));
+        }
+        Ok(Children(v.into_iter()))
+    }
+
+    /// Get reference to a node.
+    //pub fn get_node(&self, id: u64) -> FsResult<&Node<K, D>>  {
+    pub fn get_node(&self, id: u64) -> FsResult<&D>  {
+        let n = self.nodes.get(&id).ok_or(FsError::NotFound)?;
+        Ok(&n.data)
+    }
+
+    /// Get mutable reference to a node.
+    //pub fn get_node_mut(&mut self, id: u64) -> FsResult<&Node<K, D>>  {
+    pub fn get_node_mut(&mut self, id: u64) -> FsResult<&mut D>  {
+        let n = self.nodes.get_mut(&id).ok_or(FsError::NotFound)?;
+        Ok(&mut n.data)
+    }
+
+    fn delete_node_from_parent(&mut self, id: u64) -> FsResult<()> {
+        let parent_id = self.nodes.get(&id).ok_or(FsError::NotFound)?.parent_id;
+        let key = {
+            let pnode = self.nodes.get(&parent_id).unwrap();
+            let mut key = None;
+            for (k, i) in &pnode.children {
+                if i == &id {
+                    key = Some((*k).clone());
+                    break;
+                }
+            }
+            key
+        };
+        let key = key.unwrap();
+        let pnode = self.nodes.get_mut(&parent_id).unwrap();
+        pnode.children.remove(&key);
+        Ok(())
+    }
+
+    /// Delete a node. Fails if node has children. Returns node itself.
+    pub fn delete_node(&mut self, id: u64) -> FsResult<Node<K, D>> {
+        {
+            let n = self.nodes.get(&id).ok_or(FsError::NotFound)?;
+            if n.children.len() > 0 {
+                return Err(FsError::Forbidden);
+            }
+        }
+        self.delete_node_from_parent(id)?;
+        Ok(self.nodes.remove(&id).unwrap())
+    }
+
+    /// Move a node to a new position and new name in the tree.
+    /// If "overwrite" is true, will replace an existing
+    /// node, but only if it doesn't have any children.
+    pub fn move_node(&mut self, id: u64, new_parent: u64, new_name: K, overwrite: bool) -> FsResult<()> {
+        let dest = {
+            let pnode = self.nodes.get(&new_parent).ok_or(FsError::NotFound)?;
+            if let Some(cid) = pnode.children.get(&new_name) {
+                let cnode = self.nodes.get(cid).unwrap();
+                if !overwrite || cnode.children.len() > 0 {
+                    return Err(FsError::Exists);
+                }
+                Some(*cid)
+            } else {
+                None
+            }
+        };
+        self.delete_node_from_parent(id)?;
+        self.nodes.get_mut(&id).unwrap().parent_id = new_parent;
+        if let Some(dest) = dest {
+            self.nodes.remove(&dest);
+        }
+        let pnode = self.nodes.get_mut(&new_parent).unwrap();
+        pnode.children.insert(new_name, id);
+        Ok(())
+    }
+}
+
+impl<K> Iterator for Children<K> {
+    type Item = (K, u64);
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next()
+    }
+}
+
