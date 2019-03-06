8c152521c3ff15f77f5d762ff16b6aaeea6b95b8
Miquel van Smoorenburg <mike@langeraar.net>
2019-03-06 15:33:37 +0100

    remove unused code

--- src/common.rs
+++
@@ -1,9 +0,0 @@
-pub use futures::prelude::*;
-pub use futures03::compat::Future01CompatExt;
-pub use futures03::compat::Stream01CompatExt;
-pub use futures03::future::Future as Future03;
-pub use futures03::future::FutureObj as FutureObj03;
-pub use futures03::future::{FutureExt, TryFutureExt};
-pub use futures03::sink::SinkExt;
-pub use futures03::stream::Stream as Stream03;
-pub use futures03::stream::StreamExt;
