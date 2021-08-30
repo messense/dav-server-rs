65718a3ee1ece089dbbccd34ddbb6d7bf7b374c0
messense <messense@icloud.com>
2021-08-30 22:23:39 +0800

    Upgrade percent-encoding to 2.1.0

--- src/davpath.rs
+++ src/davpath.rs
@@ -10,6 +10,15 @@ use percent_encoding as pct;
 
 use crate::DavError;
 
+// Encode all non-unreserved characters, except '/'.
+// See RFC3986, and https://en.wikipedia.org/wiki/Percent-encoding .
+const PATH_ENCODE_SET: &pct::AsciiSet = &pct::NON_ALPHANUMERIC
+    .remove(b'-')
+    .remove(b'_')
+    .remove(b'.')
+    .remove(b'~')
+    .remove(b'/');
+
 /// URL path, with hidden prefix.
 #[derive(Clone)]
 pub struct DavPath {
@@ -21,26 +30,6 @@ pub struct DavPath {
 /// It's what you get when you `Deref` `DavPath`, and returned by `DavPath::with_prefix()`.
 pub struct DavPathRef {
     fullpath: [u8],
-}
-
-#[derive(Copy, Clone, Debug)]
-#[allow(non_camel_case_types)]
-struct ENCODE_SET;
-
-impl pct::EncodeSet for ENCODE_SET {
-    // Encode all non-unreserved characters, except '/'.
-    // See RFC3986, and https://en.wikipedia.org/wiki/Percent-encoding .
-    #[inline]
-    fn contains(&self, byte: u8) -> bool {
-        let unreserved = (byte >= b'A' && byte <= b'Z') ||
-            (byte >= b'a' && byte <= b'z') ||
-            (byte >= b'0' && byte <= b'9') ||
-            byte == b'-' ||
-            byte == b'_' ||
-            byte == b'.' ||
-            byte == b'~';
-        !unreserved && byte != b'/'
-    }
 }
 
 impl std::fmt::Display for DavPath {
@@ -102,7 +91,7 @@ fn valid_segment(src: &[u8]) -> Result<(), ParseError> {
 
 // encode path segment with user-defined ENCODE_SET
 fn encode_path(src: &[u8]) -> Vec<u8> {
-    pct::percent_encode(src, ENCODE_SET).to_string().into_bytes()
+    pct::percent_encode(src, PATH_ENCODE_SET).to_string().into_bytes()
 }
 
 // make path safe:
